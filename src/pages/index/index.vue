34d3844c252d0272f04768429808a69a413d79bd
chinsyoui <chinsyoui@gmail.com>
2021-11-19 23:14:17 +0800

    enable auto update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -26,6 +26,32 @@
 		mounted() {
 			// this.DebugInfo = this.DebugInfo + "uni.getSystemInfoSync() = " + JSON.stringify(uni.getSystemInfoSync());
 		},
+        onLoad() {
+            if (wx && wx.getUpdateManager) {
+                const um = wx.getUpdateManager();
+                if (um && um.onCheckForUpdate && um.onUpdateReady) {
+                    um.onCheckForUpdate(function(res) {
+                        // 请求完新版本信息的回调
+                        console.log(res.hasUpdate);
+                    });
+
+                    um.onUpdateReady(function() {
+                        wx.showModal({
+                            title: '更新提示',
+                            content: '新版本已经准备好，是否重启应用？',
+                            success(res) {
+                                if (res.confirm) {
+                                    // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                                    um.applyUpdate();
+                                }
+                            }
+                        });
+                    });
+
+                    um.onUpdateFailed(function () {});
+                }
+            }
+        },
 		methods: {
 			onItemClick: function() {
                 //console.log(this.Title + "." + "onItemClick");
